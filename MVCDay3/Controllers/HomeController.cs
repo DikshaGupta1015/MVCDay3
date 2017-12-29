b466d144f01302e44f55222103427bc2fbb09582
DikshaGupta1015 <34881041+DikshaGupta1015@users.noreply.github.com>
2017-12-29 14:36:06 +0530

    Add project files.

---
+++ MVCDay3/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDay3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index(string id, string pass, string token)
+        {
+            return "Hello!!" + DateTime.Now.ToString() + " Id =" + id;
+        }
+
+        public string Index2(string id)
+        {
+            return "This is Index 2 Hello!!" + DateTime.Now.ToString();
+        }
+
+        public ActionResult Index1()
+        {
+            ViewBag.FirstName = "Diksha";
+            return View();
+        }
+
+        public ActionResult Index3(string id)
+        {
+            TempData["FirstName"] = 23;
+            return View();
+        }
+    }
+}
---
+++ MVCDay3/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDay3.Models
+{
+    public class Department
+    {
+        public string DeptName { get; set; }
+        public int DeptId { get; set; }
+    }
+}
